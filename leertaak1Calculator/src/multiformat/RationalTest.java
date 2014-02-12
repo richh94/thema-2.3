6f8d7ee0d22f7dcf43bb803762b7e8b638807700
Richard Wemmenhove <richard_wemmenhove@live.nl>
2014-02-12 13:29:08 +0100

    opg 1.25 octodad
    OCTODAAAD

---
+++ leertaak1Calculator/src/multiformat/RationalTest.java
@@ -0,0 +1,14 @@
+package multiformat;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RationalTest {
+
+	@Test
+	public void testDiv() {
+		fail("Not yet implemented");
+	}
+
+}
